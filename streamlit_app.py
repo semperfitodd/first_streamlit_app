55e2bf23342f434ce1148b1633f18859d7740d5f
semperfitodd <todd@bernsonfamily.com>
2023-12-11 11:23:21 -0500

    updating streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import pandas
+import requests
 import streamlit
 
 streamlit.title('Todd\'s New Healthy Diner')
@@ -18,3 +19,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
